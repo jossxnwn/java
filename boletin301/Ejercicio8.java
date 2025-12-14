de17f08e24fb8740ffe791d425b1445cd7e3d7f4
jossx_xnwn <169128172+jossxnwn@users.noreply.github.com>
2025-12-14 18:18:16 +0100

    Add files via upload

---
+++ boletin301/Ejercicio8.java
@@ -0,0 +1,20 @@
+package boletin301;
+
+/**
+ * Author: Josue Francis Sayritupac Izquierdo
+ * Email: a25josuesi@iesantonlosada.gal
+ * EmailPersonal: Josue108125@gmail.com
+ * Date: 12/14/2025
+ */
+/*Ejercicio 8. Crea un programa en Java que pida un número por consola. A continuación, calcula el valor de la raíz cuadrada aproximada por debajo de ese número utilizando bucles. Por ejemplo: la raíz cuadrada de 33 sería 5, porque 5·5 = 25 y 6·6 = 36. Imprime el valor de la raíz cuadrada por pantalla. Si el valor es menor que 1, escribe Error por pantalla.*/
+
+import java.util.Scanner;
+public class Ejercicio8 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Ingresa un numero: ");
+        int numero = entrada.nextInt();
+        int div = numero;
+        while(div > numero )
+    }
+}
