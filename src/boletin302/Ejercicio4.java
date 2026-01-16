3d07f150a16197a8489b59a0b16954f329a808f2
joss <josue108125@gmail.com>
2026-01-16 14:54:14 +0100

    falta terminar la 4

--- src/boletin302/Ejercicio4.java
+++ src/boletin302/Ejercicio4.java
@@ -6,10 +6,35 @@ package boletin302;
  * EmailPersonal: Josue108125@gmail.com
  * Date: 16/1/26
  */
-/*indice*/
+/*Ejercicio 4. Se desea gestionar las notas de estudiantes de un curso mediante matrices en Java. El ejercicio consiste en almacenar las notas de varios estudiantes en diferentes módulos.
 
+Primero se indicará el número de estudiantes.
+A continuación se indicará el número de módulos.
+A continuación se añadirán las notas de cada estudiante módulo a módulo.
+Una vez introducidas las notas del alumnado se mostrará el siguiente menú utilizando la estructura switch:
+a) Media de un alumno/a: se pedirá el índice del alumno y se calculará la media de sus notas.
+b) Porcentaje de aprobados: se calculará el porcentaje de aprobados del módulo del que se introduzca el índice.*/
+import java.util.Scanner;
 public class Ejercicio4 {
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Ingresa el numero de estudiantes: ");
+        int estudiantes = entrada.nextInt();
+        System.out.print("Ingrese el numero de modulos: ");
+        int modulos = entrada.nextInt();
+        int [][] matriz = new int[estudiantes][modulos];
+        rellenarDatos(estudiantes, modulos, entrada, matriz);
+
+        //Funciones
 
     }
+    private static void rellenarDatos (int estudiantes, int modulos, Scanner entrada, int [][] matriz){
+        for (int c = 0 ; c < estudiantes ; c++ ){
+            for (int f = 0 ; f < modulos ; f++ ){
+                System.out.printf("Introduce la nota del modulo %d : ", c);
+                matriz[f][c] = entrada.nextInt();
+            }
+        }
+    }
+
 }
